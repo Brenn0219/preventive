04c87c0648091775b1b76549b8ed92e8b6696ef9
brenn0219 <brennoaugusto2109@gmail.com>
2026-03-23 16:42:55 -0300

    fix: names

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,20 @@
 URL = "https://yduqs.service-now.com/now/nav/ui/classic/params/target/u_tarefas_rotineiras_de_fieldservice_list.do"
 
-GRUPO = "SN_TI_FIELD_SERVICE_N1"
-RESPONSAVEL = "Pedro Henrique Fernandes Costa"
-LOCAL = "INSTITUTO CULTURAL NEWTON PAIVA - CARLOS LUZ"
+GROUP = "SN_TI_FIELD_SERVICE_N1"
+ASSIGNEE = "Pedro Henrique Fernandes Costa"
+LOCATION = "INSTITUTO CULTURAL NEWTON PAIVA - CARLOS LUZ"
 
-TIPO_MAP = {
+TYPE_MAP = {
     "sala": "5",
     "lab": "2"
 }
 
-DESCRICAO = {
+DESCRIPTION = {
     "sala": "Preventiva de sala de aula: verificação de equipamentos, rede e funcionamento geral.",
     "lab": "Preventiva de laboratório de informática: verificação de máquinas, rede, periféricos e funcionamento geral."
 }
 
-EXCEL_PATH = "salas.xlsx"
+EXCEL_PATH = "salas-np-cl.xlsx"
 
 MAX_RETRY = 3
 TIMEOUT = 60000
