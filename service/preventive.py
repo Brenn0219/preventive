7cd2f73c9a4a18f4d8b9bda5d5d1bb672d00a0e5
brenn0219 <brennoaugusto2109@gmail.com>
2026-03-23 17:33:53 -0300

    fix: frames not found

--- service/preventive.py
+++ service/preventive.py
@@ -1,8 +1,8 @@
 from config import settings
 
 def get_main_frame(page):
-    page.wait_for_selector("iframe[name='gsft_main']")
-    return page.frame(name="gsft_main")
+    page.wait_for_selector("iframe[name='gsft_main']", timeout=60000)
+    return page.frame_locator("iframe[name='gsft_main']")
 
 
 def fill_reference_field(frame, selector: str, value: str):
@@ -10,10 +10,7 @@ def fill_reference_field(frame, selector: str, value: str):
 
     field.click()
     field.fill(value)
-
-    # ServiceNow precisa de TAB para validar campo
-    frame.wait_for_timeout(1500)
-    field.press("Tab")
+    field.press("Tab")  
 
 
 def create_preventive(page, item: dict, logger):
@@ -24,11 +21,14 @@ def create_preventive(page, item: dict, logger):
 
     frame = get_main_frame(page)
 
-    # Aguarda botão "Novo"
-    frame.wait_for_selector("#sysverb_new", timeout=settings.TIMEOUT)
-    frame.click("#sysverb_new")
+    new_button = frame.locator("#sysverb_new")
+    new_button.wait_for()
+    new_button.click()
 
-    # Preenche campos
+    frame.locator(
+        "#sys_display\\.u_tarefas_rotineiras_de_fieldservice\\.assignment_group"
+    ).wait_for()
+
     fill_reference_field(
         frame,
         "#sys_display\\.u_tarefas_rotineiras_de_fieldservice\\.assignment_group",
@@ -47,25 +47,18 @@ def create_preventive(page, item: dict, logger):
         settings.LOCATION
     )
 
-    # Tipo (select)
-    frame.select_option(
-        "#u_tarefas_rotineiras_de_fieldservice\\.u_tipo_de_solicita_o",
-        settings.TYPE_MAP[request_type]
-    )
+    frame.locator(
+        "#u_tarefas_rotineiras_de_fieldservice\\.u_tipo_de_solicita_o"
+    ).select_option(settings.TYPE_MAP[request_type])
 
-    # Nome do setor
-    frame.fill(
-        "#u_tarefas_rotineiras_de_fieldservice\\.u_nome_do_setor",
-        sector
-    )
+    frame.locator(
+        "#u_tarefas_rotineiras_de_fieldservice\\.u_nome_do_setor"
+    ).fill(sector)
 
-    # Descrição
-    frame.fill(
-        "#u_tarefas_rotineiras_de_fieldservice\\.description",
-        settings.DESCRIPTION[request_type]
-    )
+    frame.locator(
+        "#u_tarefas_rotineiras_de_fieldservice\\.description"
+    ).fill(settings.DESCRIPTION[request_type])
 
-    # Enviar
-    frame.click("#sysverb_insert_bottom")
-
-    frame.wait_for_timeout(3000)
+    # frame.locator("#sysverb_insert_bottom").click()
+    
+    page.wait_for_load_state("networkidle")
