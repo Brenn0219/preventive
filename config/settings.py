8756f39fbcba0e3be0fdfea155ce008a2e5967a0
brenn0219 <brennoaugusto2109@gmail.com>
2026-03-23 16:17:59 -0300

    feat: settings

---
+++ config/settings.py
@@ -0,0 +1,20 @@
+URL = "https://yduqs.service-now.com/now/nav/ui/classic/params/target/u_tarefas_rotineiras_de_fieldservice_list.do"
+
+GRUPO = "SN_TI_FIELD_SERVICE_N1"
+RESPONSAVEL = "Pedro Henrique Fernandes Costa"
+LOCAL = "INSTITUTO CULTURAL NEWTON PAIVA - CARLOS LUZ"
+
+TIPO_MAP = {
+    "sala": "5",
+    "lab": "2"
+}
+
+DESCRICAO = {
+    "sala": "Preventiva de sala de aula: verificação de equipamentos, rede e funcionamento geral.",
+    "lab": "Preventiva de laboratório de informática: verificação de máquinas, rede, periféricos e funcionamento geral."
+}
+
+EXCEL_PATH = "salas.xlsx"
+
+MAX_RETRY = 3
+TIMEOUT = 60000
